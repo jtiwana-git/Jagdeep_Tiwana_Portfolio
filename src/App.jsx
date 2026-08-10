bd150b4906f98e5a52c1997befbd6bf94f394d21
Jagdeep Tiwana <jtiwana@hotmail.com>
2026-08-10 20:13:33 +0100

    changed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Navbar } from "@/layouts/Navbar";
-import { Hero } from "@/sections/Hero";
+import { Navbar } from "./layouts/Navbar";
+import { Hero } from "./sections/Hero";
 import { About } from "./sections/About";
-import { Projects } from "@/sections/Projects";
-import { Contact } from "@/sections/Contact";
+import { Projects } from "./sections/Projects";
+import { Contact } from "./sections/Contact";
 
 function App() {
   return (
